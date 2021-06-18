6eca4070cd62f66ef2355038601a22e4e2f0f9c5
Ivan Kam <ivankam1107@gmail.com>
2021-06-19 03:28:10 +0800

    Staging 1

---
+++ NetworkScanner/DNSScan.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Heijden.DNS;
+using System.Threading;
+
+namespace NetworkScanner
+{
+    class DNSScan
+    {
+        private List<Thread> threads;
+        public static void DNS(object args)
+        {
+            string ip = args as string;
+            try
+            {
+                Resolver resolver = new Resolver(Form1.dnsServerIP);
+                resolver.TimeOut = 1;
+                IPHostEntry iPHost = resolver.GetHostEntry(ip);  
+                if (iPHost.HostName != null)
+                {
+                    if (!Form1.CURRENT.ContainsKey(ip)) Form1.CURRENT.Add(ip, new Dictionary<string, SortedSet<dynamic>>());
+                    if (!Form1.CURRENT[ip].ContainsKey("Hostname")) Form1.CURRENT[ip].Add("Hostname", new SortedSet<dynamic>());
+                    Form1.CURRENT[ip]["Hostname"].Add(iPHost.HostName);
+                    Form1.DisplayTips(ip);
+                } 
+            }
+            catch
+            {
+
+            }
+        }
+        public DNSScan()
+        {
+            Form1.Status = "Dns";
+            threads = new List<Thread>();
+            foreach(string ip in Form1.CURRENT.Keys)
+            {
+                Thread thread = new Thread(DNS);
+                threads.Add(thread);
+                thread.Start(ip);
+            }
+            foreach (string ip in PingScan.failIPs)
+            {
+                Thread thread = new Thread(DNS);
+                threads.Add(thread);
+                thread.Start(ip);
+            }
+        }
+    }
+}
--- NetworkScanner/Form1.cs
+++ NetworkScanner/Form1.cs
@@ -12,43 +12,73 @@ using System.IO;
 using System.Xml;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Net.NetworkInformation;
 
 namespace NetworkScanner
 {
     public partial class Form1 : Form
     {
-        public Label[] labels;
-        public ToolTip[] tooltips;
+        public static Label[] labels;
+        public static ToolTip[] tooltips;
         private string result;
-        public Color CNotExist, CHasArp, CPingSuccess, CPortAlive;
-        public int PingTimeout, PortTimeout;
-        public List<int> portList;
-
+        public static Color CNotExist, CHasArp, CPingSuccess, CPortAlive, CDifferentHistory;
+        public static int PingTimeout, PortTimeout;
+        public static List<int> portList;
+        public Dictionary<string, Dictionary<string, Dictionary<string, SortedSet<dynamic>>>> HISTORY;
+        public static Dictionary<string, Dictionary<string, SortedSet<dynamic>>> CURRENT;
+        public static Dictionary<string, Dictionary<string, SortedSet<dynamic>>> SUBNET;
+        public static string subnet;
+        public static string dnsServerIP;
+        public static string Status;
         public Form1()
         {
             InitializeComponent();
-            init();
+            labels = new Label[256];
+            tooltips = new ToolTip[256];
+            for (int i = 0; i < 256; i++)
+            {
+                labels[i] = new Label();
+                tooltips[i] = new ToolTip();
+                /*existIPs.Add(new ExistIP());*/
+                labels[i].Text = i.ToString();
+                labels[i].Location = new Point(28 + i % 10 * 30, 10 + (i / 10) * 20); // for example
+                labels[i].AutoSize = true;
+                labels[i].DoubleClick += new EventHandler(Label_Click);
+            }
+            textBox3.Text = GetDnsAdress();
             tabControl1.TabPages[0].Controls.AddRange(labels);
+            init();
             textBox1.Select();
         }
         private void button1_Click(object sender, EventArgs e)
         {
             init();
-            GetSubnetHistory(textBox1.Text);
+            string ip = textBox1.Text;
+            subnet = IPPrefix(ip);
+            for (int i = 0; i < 256; i++)
+            {
+                PingScan.failIPs.Add(subnet + i.ToString());
+            }
+            GetSubnetHistory(ip);
+            if (checkBox1.Checked) new PingScan();
+            if (checkBox2.Checked) new PortScan();
+            if (checkBox3.Checked) new DNSScan();
+            result = IPPrefix(ip);
         }
-        public Dictionary<string, Dictionary<string,Dictionary<string, SortedSet<dynamic>>>> HISTORY;
-        public Dictionary<string, Dictionary<string, SortedSet<dynamic>>> CURRENT;
-        public Dictionary<string, Dictionary<string, SortedSet<dynamic>>> SUBNET;
+
         private void init()
         {
+            Status = "Init";
             CNotExist = ColorNotExist.BackColor;
             CHasArp = ColorHasARP.BackColor;
             CPingSuccess = ColorPingSuccess.BackColor;
             CPortAlive = ColorPortAlive.BackColor;
+            CDifferentHistory = ColorDifferentHistory.BackColor;
             PingTimeout = Convert.ToInt32(numericUpDown1.Value);
             PortTimeout = Convert.ToInt32(numericUpDown2.Value);
-            labels = new Label[256];
-            tooltips = new ToolTip[256];
+            dnsServerIP = textBox3.Text;
+            PingScan.failIPs = new List<string>();
             HISTORY = new Dictionary<string, Dictionary<string, Dictionary<string, SortedSet<dynamic>>>>();
             CURRENT = new Dictionary<string, Dictionary<string, SortedSet<dynamic>>>();
             if (File.Exists(textBox2.Text))
@@ -56,7 +86,7 @@ namespace NetworkScanner
                 try
                 {
                     string json = File.ReadAllText(textBox2.Text);
-                    HISTORY = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, SortedSet<dynamic>>>>>(json);    
+                    HISTORY = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, SortedSet<dynamic>>>>>(json);
                 }
                 catch
                 {
@@ -69,28 +99,26 @@ namespace NetworkScanner
             {
                 portList.Add(Convert.ToInt32(item));
             }
+            foreach (Label label in labels)
+            {
+                label.BackColor = DefaultBackColor;
+            }
             for (int i = 0; i < 256; i++)
             {
-                labels[i] = new Label();
+                tooltips[i].Dispose();
                 tooltips[i] = new ToolTip();
-                /*existIPs.Add(new ExistIP());*/
-                labels[i].Text = i.ToString();
-                labels[i].Location = new Point(28 + i % 10 * 30, 10 + (i / 10) * 20); // for example
-                labels[i].AutoSize = true;
-                labels[i].DoubleClick += new EventHandler(Label_Click);
             }
+
         }
         public void GetSubnetHistory(string ip)
         {
-            string subnet = IPPrefix(ip);
             try
             {
                 SUBNET = HISTORY[subnet];
-                
             }
             catch
             {
-
+                SUBNET = new Dictionary<string, Dictionary<string, SortedSet<dynamic>>>();
             }
             Console.WriteLine(JsonConvert.SerializeObject(SUBNET));
         }
@@ -101,7 +129,6 @@ namespace NetworkScanner
             //string tempJson = "{\"192.168.1.2\":{\"Hostname\":[\"iamhost\"],\"PortAlive\":[22,4431]}}";
             //CURRENT = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, SortedSet<dynamic>>>>(tempJson);
             ////////////////////////////
-            string subnet = IPPrefix(ip);
             try
             {
                 HISTORY.Add(subnet, new Dictionary<string, Dictionary<string, SortedSet<dynamic>>>());
@@ -155,7 +182,7 @@ namespace NetworkScanner
 
         private void button2_Click(object sender, EventArgs e)
         {
-            foreach(var port in listBox1.Items)
+            foreach (var port in listBox1.Items)
             {
                 if (Convert.ToInt32(port) == numericUpDown3.Value)
                 {
@@ -177,7 +204,7 @@ namespace NetworkScanner
 
         private void PortClick(object sender, EventArgs e)
         {
-            numericUpDown3.Select(0,5);
+            numericUpDown3.Select(0, 5);
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -227,7 +254,7 @@ namespace NetworkScanner
             XmlNodeList nodelist = doc.SelectNodes("/Setting");
             string[] ports = nodelist[0].SelectSingleNode("portList").InnerText.Split(',');
             listBox1.Items.Clear();
-            foreach(string port in ports)
+            foreach (string port in ports)
             {
                 listBox1.Items.Add(port);
             }
@@ -240,7 +267,7 @@ namespace NetworkScanner
             textBox2.Text = nodelist[0].SelectSingleNode("history").InnerText;
         }
 
-        private string IPPrefix(string ip)
+        public static string IPPrefix(string ip)
         {
             string[] prefix = ip.Split('.');
             return prefix[0] + "." + prefix[1] + "." + prefix[2] + ".";
@@ -264,8 +291,14 @@ namespace NetworkScanner
             {
                 pictureBox.BackColor = colorDialog1.Color;
             }
-            
+
         }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox3.Enabled = checkBox3.Checked;
+        }
+
         private void saveConfig()
         {
             string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "NetworkScanner");
@@ -280,10 +313,10 @@ namespace NetworkScanner
                 path = saveFileDialog1.InitialDirectory;
             }
             string portList = "";
-            foreach(var port in listBox1.Items)
+            foreach (var port in listBox1.Items)
             {
                 portList += port.ToString() + ',';
-            }    
+            }
             using (XmlWriter writer = XmlWriter.Create(path + filename))
             {
                 writer.WriteStartElement("Setting");
@@ -300,8 +333,8 @@ namespace NetworkScanner
                 writer.Flush();
             }
         }
-        
-        private dynamic GetXmlValue(XmlNodeList nodelist,string key)
+
+        private dynamic GetXmlValue(XmlNodeList nodelist, string key)
         {
             try
             {
@@ -311,7 +344,7 @@ namespace NetworkScanner
             {
                 return nodelist[0].SelectSingleNode(key)?.InnerText;
             }
-            
+
         }
         private bool openConfig()
         {
@@ -321,7 +354,7 @@ namespace NetworkScanner
                 string filename = @"\Setting.xml";
                 XmlDocument doc = new XmlDocument();
                 Dictionary<string, string> config = new Dictionary<string, string>();
-                doc.Load(path+filename);
+                doc.Load(path + filename);
                 XmlNodeList nodelist = doc.SelectNodes("/Setting");
                 string[] ports = nodelist[0].SelectSingleNode("portList").InnerText.Split(',');
                 listBox1.Items.Clear();
@@ -329,8 +362,8 @@ namespace NetworkScanner
                 {
                     listBox1.Items.Add(port);
                 }
-                numericUpDown1.Value = (GetXmlValue(nodelist, "pingTimeout") != 0) ? GetXmlValue(nodelist, "pingTimeout"): numericUpDown1.Value;
-                numericUpDown2.Value = (GetXmlValue(nodelist, "portTimeout") != 0) ? GetXmlValue(nodelist, "portTimeout"): numericUpDown2.Value;
+                numericUpDown1.Value = (GetXmlValue(nodelist, "pingTimeout") != 0) ? GetXmlValue(nodelist, "pingTimeout") : numericUpDown1.Value;
+                numericUpDown2.Value = (GetXmlValue(nodelist, "portTimeout") != 0) ? GetXmlValue(nodelist, "portTimeout") : numericUpDown2.Value;
                 ColorNotExist.BackColor = (GetXmlValue(nodelist, "colorNotExist") != 0) ? Color.FromArgb(GetXmlValue(nodelist, "colorNotExist")) : ColorNotExist.BackColor;
                 ColorHasARP.BackColor = (GetXmlValue(nodelist, "colorHasARP") != 0) ? Color.FromArgb(GetXmlValue(nodelist, "colorHasARP")) : ColorHasARP.BackColor;
                 ColorPingSuccess.BackColor = (GetXmlValue(nodelist, "colorPingSuccess") != 0) ? Color.FromArgb(GetXmlValue(nodelist, "colorPingSuccess")) : ColorPingSuccess.BackColor;
@@ -344,6 +377,22 @@ namespace NetworkScanner
             }
             return true;
         }
+        private static string GetDnsAdress()
+        {
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface networkInterface in networkInterfaces)
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    IPInterfaceProperties ipProperties = networkInterface.GetIPProperties();
+                    IPAddressCollection dnsAddresses = ipProperties.DnsAddresses;
+                    return dnsAddresses.First().ToString();
+                }
+            }
+
+            throw new InvalidOperationException("Unable to find DNS Address");
+        }
         private void removeList()
         {
             for (int x = listBox1.SelectedIndices.Count - 1; x >= 0; x--)
@@ -351,6 +400,74 @@ namespace NetworkScanner
                 int idx = listBox1.SelectedIndices[x];
                 listBox1.Items.RemoveAt(idx);
             }
+        }
+        public static void DisplayTips(string ip="")
+        {
+            int tag = Convert.ToInt32(ip.ToString().Split('.')[3]);
+            string tips = "";
+            if (!SUBNET.ContainsKey(ip) && CURRENT.ContainsKey(ip))
+            {
+                //show curr[ip] info
+                labels[tag].BackColor = CPingSuccess;
+                if (CURRENT[ip].ContainsKey("Mac"))
+                {
+                    labels[tag].BackColor = CHasArp;
+                    tips += String.Format("{0}", CURRENT[ip]["Mac"].ElementAt(0))+"\r\n";
+                }
+                if (CURRENT[ip].ContainsKey("Hostname"))
+                {
+                    labels[tag].BackColor = CHasArp;
+                    tips += String.Format("{0}", CURRENT[ip]["Hostname"].ElementAt(0)) + "\r\n";
+                }
+                if (CURRENT[ip].ContainsKey("PortAlive"))
+                {
+                    labels[tag].BackColor = CPortAlive;
+                    tips += String.Format("Alive Ports:{0}", JsonConvert.SerializeObject(CURRENT[ip]["PortAlive"]));
+                }
+
+            }
+            else if(SUBNET.ContainsKey(ip) && !CURRENT.ContainsKey(ip))
+            {
+                //show sub[ip] info
+                labels[tag].BackColor = CDifferentHistory;
+                tips = "History:\r\n";
+                if (SUBNET[ip].ContainsKey("Mac"))
+                {
+                    tips += String.Format("  {0}", JsonConvert.SerializeObject(SUBNET[ip]["Mac"])) + "\r\n";
+                }
+                if (SUBNET[ip].ContainsKey("Hostname"))
+                {
+                    tips += String.Format("  {0}", JsonConvert.SerializeObject(SUBNET[ip]["Hostname"])) + "\r\n";
+                }
+                if (SUBNET[ip].ContainsKey("PortAlive"))
+                {
+                    tips += String.Format("  Alive Ports:{0}", JsonConvert.SerializeObject(SUBNET[ip]["PortAlive"]));
+                }
+            }
+            else if(SUBNET.ContainsKey(ip) && CURRENT.ContainsKey(ip))
+            {
+                //Compare
+                if((CURRENT[ip].ContainsKey("Mac") && SUBNET[ip].ContainsKey("Mac")) || (CURRENT[ip].ContainsKey("Hostname") && SUBNET[ip].ContainsKey("Hostname")) || (CURRENT[ip].ContainsKey("PortAlive") && SUBNET[ip].ContainsKey("PortAlive")))
+                {
+                    labels[tag].BackColor = CDifferentHistory;
+                    tips = ShowDiif(ip);
+                }
+            }
+            else
+            {
+                return;
+            }
+            Form1.labels[tag].BeginInvoke((MethodInvoker)delegate
+            {
+                tooltips[tag].SetToolTip(Form1.labels[tag], tips);
+            });
+        }
+        private static string ShowDiif(string ip="")
+        {
+            string tips="";
+
+            
+            return tips;
         }
     }
 }
